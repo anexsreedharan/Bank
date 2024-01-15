81e1ccd5bc00e731d2fb194707233030d434a719
anexsreedharan <118161989+anexsreedharan@users.noreply.github.com>
2024-01-15 11:30:05 +0530

    added

---
+++ banking/bank/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+app_name='bank'
+urlpatterns = [
+
+    path('',views.Home,name='home'),
+    path('login/',views.login_page,name='login_page'),
+    path('register/',views.Register_page,name='register'),
+    path('form/',views.form_page,name='forms'),
+    path('final/',views.final_page,name='final')
+]
